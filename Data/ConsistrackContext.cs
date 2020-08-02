dfd5e57261f986b8b7035b811d91291e1b729032
gagangupta2488 <40420893+gagangupta2488@users.noreply.github.com>
2020-08-02 23:30:58 +0530

    context

--- Data/ConsistrackContext.cs
+++ Data/ConsistrackContext.cs
@@ -11,8 +11,11 @@ public ConsistrackContext(DbContextOptions<ConsistrackContext> opt) : base(opt)
 }
 public DbSet<SimMaster> SimMasters{ get; set; }
 public DbSet<GPSMaster> GPSMasters{ get; set; }
-
 public DbSet<SensorMaster> SensorMasters{ get; set; }
+public DbSet<UserMaster> UserMasters{ get; set; }
+public DbSet<UserRole> UserRoles{ get; set; }
+public DbSet<MenuMaster> MenuMasters{ get; set; }
+public DbSet<MenuRoleMap> MenuRoleMaps{ get; set; }
 
     }
 }
