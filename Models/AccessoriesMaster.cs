bd4b91d1ab480da52b1a53d42094375860c3d4d3
Yateesh Saxena <YATEESHSAXENA25@GMAIL.COM>
2020-08-05 00:46:07 +0530

    Accessories changes

--- Models/AccessoriesMaster.cs
+++ Models/AccessoriesMaster.cs
@@ -6,15 +6,15 @@ namespace Consistrack.Models
     {
          [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage="Pleae Enter the Name")]
         public string Name { get; set; }
         public string Description { get; set; }
+         [Required(ErrorMessage="Pleae Enter the DOP")]
         public DateTime? DOP { get; set; }
-        public string Serial_No { get; set; }
-        [Required]
+
+         [Required(ErrorMessage="Pleae Enter Quantity")]
         public string Quantity { get; set; }
-        public string S_No { get; set; }
-        public string Device_Id { get; set; }
-        public string Length { get; set; }
+
         public string Remark { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDT { get; set; }
